d39d49c467fe477dd30ee158f7cf926965e2f8ed
lsngmin <lsngmin@naver.com>
2026-05-22 02:03:06 +0900

    feat(train): export EventWeightedSmoothL1Loss

--- src/train/__init__.py
+++ src/train/__init__.py
@@ -1,5 +1,6 @@
 """Training utilities."""
 from .trainer import Trainer
 from .callbacks import EarlyStopping, BestCheckpoint
+from .losses import EventWeightedSmoothL1Loss
 
-__all__ = ['Trainer', 'EarlyStopping', 'BestCheckpoint']
+__all__ = ['Trainer', 'EarlyStopping', 'BestCheckpoint', 'EventWeightedSmoothL1Loss']
